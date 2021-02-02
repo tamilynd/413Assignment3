5ce865dc84a3de97accfe7da5deab3712abe4a3e
tamilynd <70982627+tamilynd@users.noreply.github.com>
2021-02-02 16:08:47 -0700

    Add project files.

---
+++ 413Assignment3/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using _413Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _413Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        public IActionResult MovieList()
+        {
+            return View(TempStorage.Movies.Where(movie => movie.Title != "Independence Day"));
+        }
+        [HttpPost]
+        public IActionResult MovieForm(MovieForm movie)
+        {
+            TempStorage.AddMovie(movie);
+            return View("ThankYou", movie);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
